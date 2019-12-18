392cee7cefca5ac4a04ba515f7759aeb83d3950b
Ilya Ugryumov <ilya.ugryumov@mail.ru>
2019-12-18 21:34:48 +0300

    issue #6: was added headers to classes. deleted sampleName variable from tool

--- src/test/java/com/epam/fonda/tools/impl/SortBamByReadNameTest.java
+++ src/test/java/com/epam/fonda/tools/impl/SortBamByReadNameTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2019 Sanofi and EPAM Systems, Inc. (https://www.epam.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.fonda.tools.impl;
 
 import com.epam.fonda.entity.configuration.Configuration;
@@ -26,9 +42,10 @@ class SortBamByReadNameTest extends AbstractTest {
     @BeforeEach
     void setUp() throws IOException, URISyntaxException {
         buildConfiguration();
-        sortBamByReadname = new SortBamByReadName("sampleName", "outDir",
+        sortBamByReadname = new SortBamByReadName("outDir",
                 BamFileSample.builder()
                         .bam("bam")
+                        .name("sampleName")
                         .build());
         Path path = Paths.get(this.getClass().getClassLoader()
                 .getResource(SORT_BAM_BY_READ_NAME_TEST_OUTPUT_DATA_PATH).toURI());
