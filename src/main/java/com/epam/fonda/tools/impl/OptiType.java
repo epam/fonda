6c4ea98fa7584c90462c118e7eedfbfb9d2c948e
yurysukhorukov <30953718+yurysukhorukov@users.noreply.github.com>
2020-01-29 15:36:33 +0300

    Issue #18: create OptiType tool (#41)
    * issue 18: OptiType tool was implemented

---
+++ src/main/java/com/epam/fonda/tools/impl/OptiType.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2017-2020 Sanofi and EPAM Systems, Inc. (https://www.epam.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.fonda.tools.impl;
+
+import com.epam.fonda.entity.command.BashCommand;
+import com.epam.fonda.entity.configuration.Configuration;
+import com.epam.fonda.entity.configuration.GlobalConfig;
+import com.epam.fonda.entity.configuration.GlobalConfigFormat;
+import com.epam.fonda.entity.configuration.StudyConfig;
+import com.epam.fonda.entity.configuration.StudyConfigFormat;
+import com.epam.fonda.samples.fastq.FastqFileSample;
+import com.epam.fonda.tools.Tool;
+import com.epam.fonda.tools.results.OptiTypeResult;
+import com.epam.fonda.tools.results.FastqResult;
+import com.epam.fonda.tools.results.OptiTypeOutput;
+import com.epam.fonda.utils.ToolUtils;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import static com.epam.fonda.utils.ToolUtils.validate;
+import static java.lang.String.format;
+
+@RequiredArgsConstructor
+public class OptiType implements Tool<OptiTypeResult> {
+
+    private static final String OPTY_TIPE_TOOL_TEMPLATE_NAME = "optiType_tool_template";
+
+    @NonNull
+    private FastqFileSample sample;
+    @NonNull
+    private FastqResult fastqResult;
+
+    @Data
+    @Builder
+    private static class OptiTypeFields {
+        private String optiType;
+        private String optiTypeOutDir;
+        private String sampleName;
+        private String libraryType;
+        private String python;
+        private String fastq1;
+        private String fastq2;
+        private String mhc1hlaTypeRes;
+        private String mhc1hlaCoverage;
+    }
+
+    /**
+     * This method generates bash script {@link BashCommand} for OptiType tool.
+     *
+     * @param configuration  is the type of {@link Configuration} which contains
+     *                       its fields: optiType, libraryType, python.
+     * @param templateEngine is the type of {@link TemplateEngine}.
+     * @return {@link BashCommand} with bash script.
+     **/
+    @Override
+    public OptiTypeResult generate(Configuration configuration, TemplateEngine templateEngine) {
+        final OptiTypeFields optiTypeFields = constructFieldsForOptiType(configuration);
+        if (optiTypeFields.fastq1 == null) {
+            throw new IllegalArgumentException(
+                    "Error Step: In optiType: not fastq files are properly provided, please check!");
+        }
+        OptiTypeOutput optiTypeOutput = OptiTypeOutput.builder()
+                .optiTypeOutdir(optiTypeFields.getOptiTypeOutDir())
+                .mhc1hlaTypeRes(optiTypeFields.mhc1hlaTypeRes)
+                .mhc1hlaCoverage(optiTypeFields.mhc1hlaCoverage)
+                .build();
+        optiTypeOutput.createDirectory();
+        Context context = new Context();
+        context.setVariable("optiTypeFields", optiTypeFields);
+        final String cmd = templateEngine.process(OPTY_TIPE_TOOL_TEMPLATE_NAME, context);
+        return OptiTypeResult.builder()
+                .fastqResult(fastqResult)
+                .optiTypeOutput(optiTypeOutput)
+                .command(BashCommand.withTool(cmd))
+                .build();
+    }
+
+    /**
+     * This method initializes fields of the OptiType {@link OptiType} class.
+     *
+     * @param configuration  is the type of {@link Configuration} which contains
+     *                       its fields: optiType, libraryType, python.
+     * @return {@link OptiType.OptiTypeFields} with fields.
+     **/
+    private OptiTypeFields constructFieldsForOptiType(final Configuration configuration) {
+        final String optiTypeOutDir = format("%s/optitype", sample.getSampleOutputDir());
+        final GlobalConfig.ToolConfig toolConfig = configuration.getGlobalConfig().getToolConfig();
+        final StudyConfig studyConfig = configuration.getStudyConfig();
+        return OptiTypeFields.builder()
+                .optiType(validate(toolConfig.getOptitype(), GlobalConfigFormat.OPTITYPE))
+                .optiTypeOutDir(optiTypeOutDir)
+                .sampleName(validate(sample.getName(), ToolUtils.SAMPLE_NAME))
+                .libraryType(validate(studyConfig.getLibraryType(), StudyConfigFormat.LIBRARY_TYPE))
+                .python(validate(toolConfig.getPython(), GlobalConfigFormat.PYTHON))
+                .fastq1(fastqResult.getOut().getMergedFastq1())
+                .fastq2(fastqResult.getOut().getMergedFastq2())
+                .mhc1hlaTypeRes(format("%s/%s_hla_result.tsv", optiTypeOutDir, sample.getName()))
+                .mhc1hlaCoverage(format("%s/%s_hla_coverage_plot.pdf", optiTypeOutDir, sample.getName()))
+                .build();
+    }
+}
---
+++ src/test/java/com/epam/fonda/tools/impl/OptiTypeTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2017-2020 Sanofi and EPAM Systems, Inc. (https://www.epam.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.fonda.tools.impl;
+
+import com.epam.fonda.entity.command.BashCommand;
+import com.epam.fonda.entity.configuration.Configuration;
+import com.epam.fonda.entity.configuration.GlobalConfig;
+import com.epam.fonda.entity.configuration.StudyConfig;
+import com.epam.fonda.samples.fastq.FastqFileSample;
+import com.epam.fonda.tools.results.FastqOutput;
+import com.epam.fonda.tools.results.FastqResult;
+import com.epam.fonda.tools.results.OptiTypeResult;
+import com.epam.fonda.utils.TemplateEngineUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.thymeleaf.TemplateEngine;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OptiTypeTest extends AbstractTest {
+    private static final String OPTY_TIPE_DNA_TEST_OUTPUT_DATA_PATH =
+            "templates/optiType_tool_with_dna_lt_test_output_data.txt";
+    private static final String OPTY_TIPE_RNA_TEST_OUTPUT_DATA_PATH =
+            "templates/optiType_tool_with_rna_lt_test_output_data.txt";
+    private static final String OPTY_TIPE_NO_LT_TEST_OUTPUT_DATA_PATH =
+            "templates/optiType_tool_with_no_lt_test_output_data.txt";
+    private OptiType optiType;
+    private Configuration expectedConfiguration;
+    private BashCommand expectedBashCommand;
+    private TemplateEngine expectedTemplateEngine = TemplateEngineUtils.init();
+
+    void setup(String libraryType, String pathToTemplate) throws URISyntaxException, IOException {
+        optiType = initTool();
+        expectedConfiguration = initConfiguration();
+        expectedConfiguration.getStudyConfig().setLibraryType(libraryType);
+        Path path = Paths.get(Objects.requireNonNull(
+                this.getClass().getClassLoader().getResource(pathToTemplate)).toURI());
+        String expectedCmd = readFile(path);
+        expectedBashCommand = new BashCommand(expectedCmd);
+    }
+
+    @ParameterizedTest
+    @MethodSource("initParameters")
+    void shouldGenerate(String libraryType, String pathToTemplate) throws IOException, URISyntaxException {
+        setup(libraryType, pathToTemplate);
+        OptiTypeResult optiTypeResult = optiType.generate(expectedConfiguration, expectedTemplateEngine);
+        assertEquals(expectedBashCommand.getToolCommand(), optiTypeResult.getCommand().getToolCommand());
+    }
+
+    @SuppressWarnings("PMD")
+    private static Stream<Arguments> initParameters() {
+        return Stream.of(
+                Arguments.of("DNA", OPTY_TIPE_DNA_TEST_OUTPUT_DATA_PATH),
+                Arguments.of("RNA", OPTY_TIPE_RNA_TEST_OUTPUT_DATA_PATH),
+                Arguments.of("NULL", OPTY_TIPE_NO_LT_TEST_OUTPUT_DATA_PATH)
+        );
+    }
+
+    private OptiType initTool() {
+        FastqFileSample expectedSample = new FastqFileSample();
+        expectedSample.setName("sampleName");
+        expectedSample.setSampleOutputDir("output");
+        expectedSample.createDirectory();
+        FastqOutput fastqOutput = FastqOutput.builder()
+                .mergedFastq1("merged_fastq1.gz")
+                .mergedFastq2("merged_fastq2.gz")
+                .build();
+        FastqResult fastqResult = FastqResult.builder()
+                .out(fastqOutput)
+                .build();
+        return new OptiType(expectedSample, fastqResult);
+    }
+
+    private Configuration initConfiguration() {
+        Configuration configuration = new Configuration();
+        GlobalConfig expectedGlobalConfig = new GlobalConfig();
+        StudyConfig expectedStudyConfig = new StudyConfig();
+        GlobalConfig.QueueParameters expectedQueueParameters = new GlobalConfig.QueueParameters();
+        expectedQueueParameters.setNumThreads(5);
+        GlobalConfig.ToolConfig expectedToolConfig = new GlobalConfig.ToolConfig();
+        expectedToolConfig.setOptitype("optiType");
+        expectedToolConfig.setPython("python");
+        expectedGlobalConfig.setQueueParameters(expectedQueueParameters);
+        expectedGlobalConfig.setToolConfig(expectedToolConfig);
+        configuration.setGlobalConfig(expectedGlobalConfig);
+        configuration.setStudyConfig(expectedStudyConfig);
+        return configuration;
+    }
+}
