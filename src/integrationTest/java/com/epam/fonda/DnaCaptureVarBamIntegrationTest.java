f238fb6a9d56f938a97d03dff3ea90e6abe1fbb1
Denis Tegai <denis_tegai@epam.com>
2020-02-06 18:31:19 +0300

    issue #48: add templates, add test case, fix task name. WIP

--- src/integrationTest/java/com/epam/fonda/DnaCaptureVarBamIntegrationTest.java
+++ src/integrationTest/java/com/epam/fonda/DnaCaptureVarBamIntegrationTest.java
@@ -15,42 +15,87 @@
  */
 package com.epam.fonda;
 
-import com.epam.fonda.workflow.TaskContainer;
+import com.epam.fonda.utils.TemplateEngineUtils;
 import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static com.epam.fonda.utils.PipelineUtils.getExecutionPath;
+import static java.lang.String.format;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(DataProviderRunner.class)
+//@RunWith(DataProviderRunner.class)
 public class DnaCaptureVarBamIntegrationTest extends AbstractIntegrationTest {
 
     private static final String OUTPUT_DIR = "output";
+    private static final String OUTPUT_DIR_ROOT = "build/resources/integrationTest/";
     private static final String QSUB = "qsub ";
     private static final String NULL = "null";
-    private static final String SH_FILES = "build/resources/integrationTest/output/sh_files/";
+    private static final String SH_FILES = "/sh_files";
     private static final String PYTHON_VCF_SNPEFF_ANNOTATION =
             ".*?/usr/bin/python.+?vcf_snpeff_annotation.py.+?(\\n|$)";
+    public static final String NA_ALL_TASKS_FOLDER = "DnaCaptureVarBam/testControlSampleNAAllTasks";
+    public static final String NA_VARDICT_FOR_GA_5_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_vardict_for_GA5_analysis_test", NA_ALL_TASKS_FOLDER);
+    public static final String NA_MUTECT_1_FOR_GA_5_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_mutect1_for_GA5_analysis_test", NA_ALL_TASKS_FOLDER);
+    public static final String NA_STRELKA_2_FOR_GA_5_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_strelka2_for_GA5_analysis_test", NA_ALL_TASKS_FOLDER);
+    public static final String NA_SCALPEL_FOR_GA_5_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_scalpel_for_GA5_analysis_test", NA_ALL_TASKS_FOLDER);
+    public static final String NA_FREEBAYES_FOR_GA_5_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_freebayes_for_GA5_analysis_test", NA_ALL_TASKS_FOLDER);
+    public static final String NA_GATK_HAPLOTYPE_CALLER_FOR_GA_5_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_gatkHaplotypeCaller_for_GA5_analysis_test", NA_ALL_TASKS_FOLDER);
+    public static final String NA_LOFREQ_FOR_GA_5_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_lofreq_for_GA5_analysis_test", NA_ALL_TASKS_FOLDER);
+    public static final String NA_VARIANT_DETECTION_FOR_GA_5_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_variantDetection_for_GA5_analysis_test", NA_ALL_TASKS_FOLDER);
 
-    @After
-    public void clean() {
-        TaskContainer.getTasks().clear();
-    }
+    public static final String NOT_NA_ALL_TASKS_FOLDER = "DnaCaptureVarBam/testControlSampleNotNAAllTasks";
+    public static final String CONT_EST_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_contEst_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    public static final String EXOMECNV_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_exomecnv_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    public static final String LOFREQ_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_lofreq_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    public static final String MUTECT_2_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_mutect2_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    public static final String SCALPEL_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_scalpel_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    public static final String SEQUENZA_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_sequenza_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    public static final String STRELKA_2_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_strelka2_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    public static final String VARDICT_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_vardict_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    public static final String VARIANT_DETECTION_FOR_GA_51_ANALYSIS_TEST = String.format(
+            "%s/DnaCaptureVar_Bam_variantDetection_for_GA51_analysis_test", NOT_NA_ALL_TASKS_FOLDER);
+    private static Context context;
+    private TemplateEngine templateEngine = TemplateEngineUtils.init();
 
     @Test
     @UseDataProvider("controlSampleNAAllTasks")
@@ -96,8 +141,68 @@ public class DnaCaptureVarBamIntegrationTest extends AbstractIntegrationTest {
                     QSUB + SH_FILES + "DnaCaptureVar_Bam_freebayes_for_GA5_analysis.sh")));
             assertTrue(lines.stream().noneMatch(line -> line.contains(NULL)));
         }
-
+        Assertions.assertAll(
+                () -> assertTrue(new File(format("%s%s/err_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/GA5", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/log_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/sh_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists())
+        );
         cleanOutputDirForNextTest(OUTPUT_DIR, false);
+    }
+
+    @BeforeEach
+    public void setup() {
+        context = new Context();
+        context.setVariable("jarPath", getExecutionPath());
+    }
+
+    @ParameterizedTest
+    @MethodSource("initToolAndTemplateNAAllTasks")
+    public void testControlSampleNAAllTasks(String task, String template) throws IOException, URISyntaxException {
+        startAppWithConfigs("DnaCaptureVarBam/gSingleAllTasks.txt", "DnaCaptureVarBam/sSingle.txt");
+        String filePath = String.format("output/sh_files/DnaCaptureVar_Bam_%s_for_GA5_analysis.sh", task);
+
+        final String expectedCmd = templateEngine.process(template, context);
+
+        assertEquals(expectedCmd.trim(), getCmd(filePath).trim());
+    }
+
+    @SuppressWarnings("PMD")
+    private static Stream<Arguments> initToolAndTemplateNAAllTasks() {
+        return Stream.of(
+                Arguments.of("vardict", NA_VARDICT_FOR_GA_5_ANALYSIS_TEST),
+                Arguments.of("mutect1", NA_MUTECT_1_FOR_GA_5_ANALYSIS_TEST),
+                Arguments.of("strelka2", NA_STRELKA_2_FOR_GA_5_ANALYSIS_TEST),
+                Arguments.of("scalpel", NA_SCALPEL_FOR_GA_5_ANALYSIS_TEST),
+                Arguments.of("freebayes", NA_FREEBAYES_FOR_GA_5_ANALYSIS_TEST),
+                Arguments.of("gatkHaplotypeCaller", NA_GATK_HAPLOTYPE_CALLER_FOR_GA_5_ANALYSIS_TEST),
+                Arguments.of("lofreq", NA_LOFREQ_FOR_GA_5_ANALYSIS_TEST),
+                Arguments.of("variantDetection", NA_VARIANT_DETECTION_FOR_GA_5_ANALYSIS_TEST)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("initToolAndTemplateNotNAAllTasks")
+    public void testControlSampleNotNAAllTasks(String task, String template) throws IOException, URISyntaxException {
+        startAppWithConfigs("DnaCaptureVarBam/gSingleAllTasks.txt", "DnaCaptureVarBam/sSingle.txt");
+        String filePath = String.format("output/sh_files/DnaCaptureVar_Bam_%s_for_GA5_analysis.sh", task);
+        final String expectedCmd = templateEngine.process(template, context);
+
+        assertEquals(expectedCmd.trim(), getCmd(filePath).trim());
+    }
+
+    private static Stream<Arguments> initToolAndTemplateNotNAAllTasks() {
+        return Stream.of(
+                Arguments.of("contEst", CONT_EST_FOR_GA_51_ANALYSIS_TEST),
+                Arguments.of("exomecnv", EXOMECNV_FOR_GA_51_ANALYSIS_TEST),
+                Arguments.of("lofreq", LOFREQ_FOR_GA_51_ANALYSIS_TEST),
+                Arguments.of("mutect2", MUTECT_2_FOR_GA_51_ANALYSIS_TEST),
+                Arguments.of("scalpel", SCALPEL_FOR_GA_51_ANALYSIS_TEST),
+                Arguments.of("sequenza", SEQUENZA_FOR_GA_51_ANALYSIS_TEST),
+                Arguments.of("strelka2", STRELKA_2_FOR_GA_51_ANALYSIS_TEST),
+                Arguments.of("vardict", VARDICT_FOR_GA_51_ANALYSIS_TEST),
+                Arguments.of("variantDetection", VARIANT_DETECTION_FOR_GA_51_ANALYSIS_TEST)
+        );
     }
 
     @DataProvider
@@ -225,7 +330,13 @@ public class DnaCaptureVarBamIntegrationTest extends AbstractIntegrationTest {
                     QSUB + SH_FILES + "DnaCaptureVar_Bam_exomecnv_for_GA51_analysis.sh")));
             assertTrue(lines.stream().noneMatch(line -> line.contains(NULL)));
         }
-
+        Assertions.assertAll(
+                () -> assertTrue(new File(format("%s%s/err_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/GA51", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/GA52", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/log_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/sh_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists())
+        );
         cleanOutputDirForNextTest(OUTPUT_DIR, false);
     }
 
@@ -360,18 +471,40 @@ public class DnaCaptureVarBamIntegrationTest extends AbstractIntegrationTest {
     }
 
     @Test
-    public void testNoTumorOrCase() throws IOException {
+    public void testNoTumorOrCase() throws IOException, URISyntaxException {
         startAppWithConfigs(
                 "DnaCaptureVarBam/gSingleAllTasks.txt",
                 "DnaCaptureVarBam/sSingleNotTumorOrCase.txt");
 
         List<File> outputFiles = Files.walk(Paths.get(
-                this.getClass().getClassLoader().getResource(OUTPUT_DIR).getPath()),
+                Objects.requireNonNull(this.getClass().getClassLoader().getResource(OUTPUT_DIR)).toURI()),
                 FileVisitOption.FOLLOW_LINKS)
                 .map(Path::toFile)
                 .sorted(Comparator.reverseOrder())
                 .filter(File::isFile)
                 .collect(Collectors.toList());
         assertTrue(outputFiles.isEmpty());
+        Assertions.assertAll(
+                () -> assertTrue(new File(format("%s%s/err_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/log_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/sh_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists())
+        );
     }
+
+
+
+
+    @Test
+    public void testOutputDirectories() throws IOException {
+        startAppWithConfigs(
+                "DnaCaptureVarBam/gSingleAllTasks.txt",
+                "DnaCaptureVarBam/sSingleNotTumorOrCase.txt");
+        Assertions.assertAll(
+                () -> assertTrue(new File(format("%s%s/err_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/log_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+                () -> assertTrue(new File(format("%s%s/sh_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists())
+        );
+        cleanOutputDirForNextTest(OUTPUT_DIR, false);
+    }
+
 }
