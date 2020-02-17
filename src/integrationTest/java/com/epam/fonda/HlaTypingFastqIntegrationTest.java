8576837cb6c24bdfaea8685618b72e9de223e40a
Iurii Sukhorukov <sukhorukovyury@gmail.com>
2020-02-17 19:34:18 +0300

    issue 17: add second test case, fix templates

--- src/integrationTest/java/com/epam/fonda/HlaTypingFastqIntegrationTest.java
+++ src/integrationTest/java/com/epam/fonda/HlaTypingFastqIntegrationTest.java
@@ -32,9 +32,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HlaTypingFastqIntegrationTest extends AbstractIntegrationTest {
 
     private static final String OUTPUT_DIR = "output";
-    private static final String G_CONFIG_PATH = "HlaTypingFastq/gPairedSeqpurgeOptiType.txt";
+    private static final String G_CONFIG_PAIRED_SEQPURGE_OPTITYPE_PATH = "HlaTypingFastq/gPairedSeqpurgeOptiType.txt";
+    private static final String G_CONFIG_PAIRED_TRIMM_QC_XENOME_YES_PATH = "HlaTypingFastq/gPairedTrimmQcXenomeYes.txt";
     private static final String S_CONFIG_PATH = "HlaTypingFastq/sHlaTypingFastq.txt";
-    private static final String HLA_TYPE_FASTQ_SUFFIX = "HlaTypingFastq/";
+    private static final String HLA_TYPING_PAIRED_SEQPURGE_OPTITYPE_SUFFIX =
+            "HlaTypingFastq/HlaTypingFastqPairedSeqpurgeOptiType/";
+    private static final String HLA_TYPE_PAIRED_TRIMM_QC_XENOME_YES_SUFFIX =
+            "HlaTypingFastq/HlaTypingFastqPairedTrimmQcXenomeYes/";
     private static final String OUTPUT_SH_FILE_SUFFIX = "output/sh_files/";
     private static final String SH_EXTENSION_SUFFIX = ".sh";
     private static final String TXT_EXTENSION_SUFFIX = ".txt";
@@ -51,42 +55,96 @@ public class HlaTypingFastqIntegrationTest extends AbstractIntegrationTest {
     }
 
     @ParameterizedTest(name = "{1}-test")
-    @MethodSource("initParameters")
-    void testHlaTypingFastq(final String outputShFile, final String templatePath)
+    @MethodSource("initParametersPairedSeqpurgeOptiType")
+    void testHlaTypingPairedSeqpurgeOptiType(final String outputShFile, final String templatePath)
             throws IOException, URISyntaxException {
-        startAppWithConfigs(G_CONFIG_PATH, S_CONFIG_PATH);
+        startAppWithConfigs(G_CONFIG_PAIRED_SEQPURGE_OPTITYPE_PATH, S_CONFIG_PATH);
+        final String expectedCmd = templateEngine.process(templatePath, context);
+        assertEquals(expectedCmd.trim(), getCmd(outputShFile).trim());
+    }
+
+    @ParameterizedTest(name = "{1}-test")
+    @MethodSource("initParametersPairedTrimmQcXenomeYes")
+    void testHlaTypingPairedTrimmQcXenomeYes(final String outputShFile, final String templatePath)
+            throws IOException, URISyntaxException {
+        startAppWithConfigs(G_CONFIG_PAIRED_TRIMM_QC_XENOME_YES_PATH, S_CONFIG_PATH);
         final String expectedCmd = templateEngine.process(templatePath, context);
         assertEquals(expectedCmd.trim(), getCmd(outputShFile).trim());
     }
 
     @SuppressWarnings("PMD")
-    private static Stream<Arguments> initParameters() {
+    private static Stream<Arguments> initParametersPairedSeqpurgeOptiType() {
         return Stream.of(
                 Arguments.of(
                         String.format("%s%s%s",
                                 OUTPUT_SH_FILE_SUFFIX, TEMPLATE_OUTPUT_SMV1_SUFFIX, SH_EXTENSION_SUFFIX),
                         String.format("%s%s%s",
-                                HLA_TYPE_FASTQ_SUFFIX, TEMPLATE_OUTPUT_SMV1_SUFFIX, TXT_EXTENSION_SUFFIX)
+                                HLA_TYPING_PAIRED_SEQPURGE_OPTITYPE_SUFFIX,
+                                TEMPLATE_OUTPUT_SMV1_SUFFIX,
+                                TXT_EXTENSION_SUFFIX)
                 ),
                 Arguments.of(
                         String.format("%s%s%s",
                                 OUTPUT_SH_FILE_SUFFIX, TEMPLATE_OUTPUT_SMV2_SUFFIX, SH_EXTENSION_SUFFIX),
                         String.format("%s%s%s",
-                                HLA_TYPE_FASTQ_SUFFIX, TEMPLATE_OUTPUT_SMV2_SUFFIX, TXT_EXTENSION_SUFFIX)
+                                HLA_TYPING_PAIRED_SEQPURGE_OPTITYPE_SUFFIX,
+                                TEMPLATE_OUTPUT_SMV2_SUFFIX,
+                                TXT_EXTENSION_SUFFIX)
                 ),
                 Arguments.of(
                         String.format("%s%s%s",
                                 OUTPUT_SH_FILE_SUFFIX, TEMPLATE_OUTPUT_SMV3_SUFFIX, SH_EXTENSION_SUFFIX),
                         String.format("%s%s%s",
-                                HLA_TYPE_FASTQ_SUFFIX, TEMPLATE_OUTPUT_SMV3_SUFFIX, TXT_EXTENSION_SUFFIX)
+                                HLA_TYPING_PAIRED_SEQPURGE_OPTITYPE_SUFFIX,
+                                TEMPLATE_OUTPUT_SMV3_SUFFIX,
+                                TXT_EXTENSION_SUFFIX)
                 ),
                 Arguments.of(
                         String.format("%s%s%s",
                                 OUTPUT_SH_FILE_SUFFIX, TEMPLATE_OUTPUT_SMV5_SUFFIX, SH_EXTENSION_SUFFIX),
                         String.format("%s%s%s",
-                                HLA_TYPE_FASTQ_SUFFIX, TEMPLATE_OUTPUT_SMV5_SUFFIX, TXT_EXTENSION_SUFFIX)
+                                HLA_TYPING_PAIRED_SEQPURGE_OPTITYPE_SUFFIX,
+                                TEMPLATE_OUTPUT_SMV5_SUFFIX,
+                                TXT_EXTENSION_SUFFIX)
                 )
         );
     }
 
+    @SuppressWarnings("PMD")
+    private static Stream<Arguments> initParametersPairedTrimmQcXenomeYes() {
+        return Stream.of(
+                Arguments.of(
+                        String.format("%s%s%s",
+                                OUTPUT_SH_FILE_SUFFIX, TEMPLATE_OUTPUT_SMV1_SUFFIX, SH_EXTENSION_SUFFIX),
+                        String.format("%s%s%s",
+                                HLA_TYPE_PAIRED_TRIMM_QC_XENOME_YES_SUFFIX,
+                                TEMPLATE_OUTPUT_SMV1_SUFFIX,
+                                TXT_EXTENSION_SUFFIX)
+                ),
+                Arguments.of(
+                        String.format("%s%s%s",
+                                OUTPUT_SH_FILE_SUFFIX, TEMPLATE_OUTPUT_SMV2_SUFFIX, SH_EXTENSION_SUFFIX),
+                        String.format("%s%s%s",
+                                HLA_TYPE_PAIRED_TRIMM_QC_XENOME_YES_SUFFIX,
+                                TEMPLATE_OUTPUT_SMV2_SUFFIX,
+                                TXT_EXTENSION_SUFFIX)
+                ),
+                Arguments.of(
+                        String.format("%s%s%s",
+                                OUTPUT_SH_FILE_SUFFIX, TEMPLATE_OUTPUT_SMV3_SUFFIX, SH_EXTENSION_SUFFIX),
+                        String.format("%s%s%s",
+                                HLA_TYPE_PAIRED_TRIMM_QC_XENOME_YES_SUFFIX,
+                                TEMPLATE_OUTPUT_SMV3_SUFFIX,
+                                TXT_EXTENSION_SUFFIX)
+                ),
+                Arguments.of(
+                        String.format("%s%s%s",
+                                OUTPUT_SH_FILE_SUFFIX, TEMPLATE_OUTPUT_SMV5_SUFFIX, SH_EXTENSION_SUFFIX),
+                        String.format("%s%s%s",
+                                HLA_TYPE_PAIRED_TRIMM_QC_XENOME_YES_SUFFIX,
+                                TEMPLATE_OUTPUT_SMV5_SUFFIX,
+                                TXT_EXTENSION_SUFFIX)
+                )
+        );
+    }
 }
