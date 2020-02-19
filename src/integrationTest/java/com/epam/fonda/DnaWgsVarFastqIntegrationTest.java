92396fbce49d5d6675f703d6083ac93664ea8fe4
Karim Magomedov <karim_magomedov@epam.com>
2020-02-19 19:50:49 +0300

    Issue #24: implement DnaWgsVar_Fastq workflow, add the test and related templates

---
+++ src/integrationTest/java/com/epam/fonda/DnaWgsVarFastqIntegrationTest.java
@@ -0,0 +1,109 @@
+package com.epam.fonda;
+
+import com.epam.fonda.utils.TemplateEngineUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.stream.Stream;
+
+import static com.epam.fonda.utils.PipelineUtils.getExecutionPath;
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DnaWgsVarFastqIntegrationTest extends AbstractIntegrationTest {
+
+    private static final String OUTPUT_DIR = "output";
+    private static final String OUTPUT_DIR_ROOT = "build/resources/integrationTest/";
+
+    private static final String DNA_WGS_VAR_FASTQ_ALIGNMENT_FOR_GA_5_1_SH_FILE_PATH =
+        "output/sh_files/DnaWgsVar_Fastq_alignment_for_GA5_1_analysis.sh";
+    private static final String DNA_WGS_VAR_FASTQ_GET_HAPLOTYPE_CALLER_FOR_GA5_SH_FILE_PATH =
+        "output/sh_files/DnaWgsVar_Fastq_gatkHaplotypeCaller_for_GA5_analysis.sh";
+    private static final String DNA_WGS_VAR_FASTQ_MERGE_MUTATION_FOR_COHORT_SH_FILE_PATH =
+        "output/sh_files/DnaWgsVar_Fastq_mergeMutation_for_cohort_analysis.sh";
+    private static final String DNA_WGS_VAR_FASTQ_POSTALIGNMENT_FOR_GA5_SH_FILE_PATH =
+        "output/sh_files/DnaWgsVar_Fastq_postalignment_for_GA5_analysis.sh";
+    private static final String DNA_WGS_VAR_FASTQ_QCSUMMARY_FOR_COHORT_SH_FILE_PATH =
+        "output/sh_files/DnaWgsVar_Fastq_qcsummary_for_cohort_analysis.sh";
+
+    private static final String DNA_WGS_VAR_FASTQ_ALIGNMENT_FOR_GA_5_1_TEMPLATE_PATH =
+        "DnaWgsVarFastq/DnaWgsVar_Fastq_alignment_for_GA5_1_analysis_template.txt";
+    private static final String DNA_WGS_VAR_FASTQ_GET_HAPLOTYPE_CALLER_FOR_GA5_TEMPLATE_PATH =
+        "DnaWgsVarFastq/DnaWgsVar_Fastq_gatkHaplotypeCaller_for_GA5_analysis_template.txt";
+    private static final String DNA_WGS_VAR_FASTQ_MERGE_MUTATION_FOR_COHORT_TEMPLATE_PATH =
+        "DnaWgsVarFastq/DnaWgsVar_Fastq_mergeMutation_for_cohort_analysis_template.txt";
+    private static final String DNA_WGS_VAR_FASTQ_POSTALIGNMENT_FOR_GA5_TEMPLATE_PATH =
+        "DnaWgsVarFastq/DnaWgsVar_Fastq_postalignment_for_GA5_analysis_template.txt";
+    private static final String DNA_WGS_VAR_FASTQ_QCSUMMARY_FOR_COHORT_TEMPLATE_PATH =
+        "DnaWgsVarFastq/DnaWgsVar_Fastq_qcsummary_for_cohort_analysis_template.txt";
+
+    public static final String DNA_WGS_VAR_FASTQ_GLOBAL_CONFIG_PATH =
+        "DnaWgsVarFastq/global_config_DnaWgsVar_Fastq_v1.1.txt";
+    public static final String STUDY_CONFIG_PATH = "DnaWgsVarFastq/sscRnaExpressionCellRangerFastq.txt";
+
+    private final TemplateEngine expectedTemplateEngine = TemplateEngineUtils.init();
+    private final Context context = new Context();
+
+    @BeforeEach
+    void setUp() {
+        context.setVariable("jarPath", getExecutionPath());
+    }
+
+    @AfterEach
+    void cleanUp() throws IOException {
+        cleanOutputDirForNextTest(OUTPUT_DIR, false);
+    }
+
+    @ParameterizedTest
+    @MethodSource("initParameters")
+    void testControlSample(String templatePath, String filePath) throws IOException, URISyntaxException {
+        startAppWithConfigs(DNA_WGS_VAR_FASTQ_GLOBAL_CONFIG_PATH, STUDY_CONFIG_PATH);
+        String expectedCmd = expectedTemplateEngine.process(templatePath, context);
+        assertEquals(expectedCmd.trim(), getCmd(filePath).trim());
+    }
+
+    @SuppressWarnings("PMD")
+    private static Stream<Arguments> initParameters() {
+        return Stream.of(
+            Arguments.of(DNA_WGS_VAR_FASTQ_ALIGNMENT_FOR_GA_5_1_TEMPLATE_PATH,
+                DNA_WGS_VAR_FASTQ_ALIGNMENT_FOR_GA_5_1_SH_FILE_PATH),
+            Arguments.of(DNA_WGS_VAR_FASTQ_GET_HAPLOTYPE_CALLER_FOR_GA5_TEMPLATE_PATH,
+                DNA_WGS_VAR_FASTQ_GET_HAPLOTYPE_CALLER_FOR_GA5_SH_FILE_PATH),
+            Arguments.of(DNA_WGS_VAR_FASTQ_MERGE_MUTATION_FOR_COHORT_TEMPLATE_PATH,
+                DNA_WGS_VAR_FASTQ_MERGE_MUTATION_FOR_COHORT_SH_FILE_PATH),
+            Arguments.of(DNA_WGS_VAR_FASTQ_POSTALIGNMENT_FOR_GA5_TEMPLATE_PATH,
+                DNA_WGS_VAR_FASTQ_POSTALIGNMENT_FOR_GA5_SH_FILE_PATH),
+            Arguments.of(DNA_WGS_VAR_FASTQ_QCSUMMARY_FOR_COHORT_TEMPLATE_PATH,
+                DNA_WGS_VAR_FASTQ_QCSUMMARY_FOR_COHORT_SH_FILE_PATH)
+        );
+    }
+
+    @Test
+    void testDir() {
+        startAppWithConfigs(DNA_WGS_VAR_FASTQ_GLOBAL_CONFIG_PATH, STUDY_CONFIG_PATH);
+        assertAll(
+            () -> assertTrue(new File(format("%s%s/sh_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(new File(format("%s%s/log_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(new File(format("%s%s/err_files", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(new File(format("%s%s/GA5", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(new File(format("%s%s/GA5/bam", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(new File(format("%s%s/GA5/fastq", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(new File(format("%s%s/GA5/gatkHaplotypeCaller", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(
+                new File(format("%s%s/GA5/gatkHaplotypeCaller/tmp", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(new File(format("%s%s/GA5/qc", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists()),
+            () -> assertTrue(new File(format("%s%s/GA5/tmp", OUTPUT_DIR_ROOT, OUTPUT_DIR)).exists())
+        );
+    }
+}
