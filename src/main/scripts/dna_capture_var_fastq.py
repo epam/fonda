98a54058009654bf08b3f2159ebb61daa324dda2
Yulia Kamyshova <yulia_kamyshova@epam.com>
2020-05-26 13:15:28 +0300

    DnaCaptureVar_Fastq workflow middleware was added

--- src/main/scripts/dna_capture_var_fastq.py
+++ src/main/scripts/dna_capture_var_fastq.py
@@ -21,9 +21,9 @@ from model.global_config import GlobalConfig
 from launcher import Launcher
 from model.study_config import StudyConfig
 
-WORKFLOW_NAME = "RnaExpression_Fastq"
-TEMPLATE = "global_template_RnaExpressionFastq.txt"
-GLOBAL_CONFIG_TOOL_TEMPLATE_NAME = "RnaExpression_tool"
+WORKFLOW_NAME = "DnaCaptureVar_Fastq"
+GLOBAL_CONFIG_TOOL_TEMPLATE_NAME = "DnaCaptureVar_Fastq_tool"
+TEMPLATE = "global_template_DnaCaptureVar_Fastq.txt"
 
 
 def usage():
@@ -33,11 +33,8 @@ def usage():
     print('	-j <job_name>                    The job ID.\n')
     print('	-d <dir_out> (required)          The output directory for the analysis.\n')
     print('	-f <fastq_list> (required)       The path to the input manifest file.\n')
-    print('	-c <cufflinks_library_type> (required)          '
-          'The cufflinks library type (fr-unstranded | fr-firststrand | fr-secondstrand).\n')
-    print('	-l <library_type>                The sequencing library type: DNAWholeExomeSeq_Paired, '
-          'DNAWholeExomeSeq_Single, DNATargetSeq_Paired, DNATargetSeq_Single, DNAAmpliconSeq_Paired, RNASeq_Paired, '
-          'RNASeq_Single, etc.\n')
+    print('	-l <library_type> (required)     The sequencing library type: DNAWholeExomeSeq_Paired, '
+          'DNAWholeExomeSeq_Single, DNATargetSeq_Paired, DNATargetSeq_Single, DNAAmpliconSeq_Paired, etc.\n')
     print('	-p <project>                     The project ID.\n')
     print('	-r <run>                         The run ID.\n')
     print('	-n <toolset> (required)          A number of tools to run in a specific pipeline.\n')
@@ -50,22 +47,19 @@ def parse_arguments(script_name, argv):
     job_name = None
     dir_out = None
     fastq_list = None
-    cufflinks_library_type = None
     library_type = None
     project = None
     run = None
     toolset = None
     flag_xenome = None
     try:
-        opts, args = getopt.getopt(argv, "hs:t:j:d:f:c:l:p:r:n:x:", ["help", "species=", "read_type=", "job_name=",
-                                                                     "dir_out=", "fastq_list=",
-                                                                     "cufflinks_library_type=", "library_type=",
-                                                                     "project=", "run=", "toolset=", "flag_xenome="])
+        opts, args = getopt.getopt(argv, "hs:t:j:d:f:l:p:r:n:x:", ["help", "species=", "read_type=", "job_name=",
+                                                                   "dir_out=", "fastq_list=", "library_type=",
+                                                                   "project=", "run=", "toolset=", "flag_xenome="])
         for opt, arg in opts:
             if opt == '-h':
                 print(script_name + ' -s <species> -t <read_type> -j <job_name> -d <dir_out> -f <fastq_list> '
-                                    '-c <cufflinks_library_type> -l <library_type> -p <project> -r <run> -n <toolset> '
-                                    '-x <flag_xenome>')
+                                    '-l <library_type> -p <project> -r <run> -n <toolset> -x <flag_xenome>')
                 sys.exit()
             elif opt in ("-s", "--species"):
                 species = arg
@@ -77,8 +71,6 @@ def parse_arguments(script_name, argv):
                 dir_out = arg
             elif opt in ("-f", "--fastq_list"):
                 fastq_list = arg
-            elif opt in ("-c", "--cufflinks_library_type"):
-                cufflinks_library_type = arg
             elif opt in ("-l", "--library_type"):
                 library_type = arg
             elif opt in ("-p", "--project"):
@@ -105,15 +97,15 @@ def parse_arguments(script_name, argv):
             print('The output directory for the analysis (-d <dir_out>) is required')
             usage()
             sys.exit(2)
-        if not cufflinks_library_type:
-            print('The cufflinks library type (-c <cufflinks_library_type>) is required')
-            usage()
-            sys.exit(2)
         if not toolset:
             print('The set of tools (-n <toolset>) is required')
             usage()
             sys.exit(2)
-        return species, read_type, job_name, dir_out, fastq_list, cufflinks_library_type, library_type, project, run, \
+        if not library_type:
+            print('The library type (-l <library_type>) is required')
+            usage()
+            sys.exit(2)
+        return species, read_type, job_name, dir_out, fastq_list, library_type, project, run, \
             toolset, flag_xenome
     except getopt.GetoptError:
         usage()
@@ -121,11 +113,9 @@ def parse_arguments(script_name, argv):
 
 
 def main(script_name, argv):
-    species, read_type, job_name, dir_out, fastq_list, cufflinks_library_type, library_type, project, run, toolset, \
-        flag_xenome = parse_arguments(script_name, argv)
+    species, read_type, job_name, dir_out, fastq_list, library_type, project, run, toolset, flag_xenome = \
+        parse_arguments(script_name, argv)
 
-    if not library_type:
-        library_type = "RNASeq"
     if not job_name:
         job_name = "{}_job".format(library_type)
     if not run:
@@ -135,8 +125,7 @@ def main(script_name, argv):
     if os.path.isdir(fastq_list):
         fastq_list = FastqSampleManifest(read_type).create(fastq_list, WORKFLOW_NAME, library_type)
 
-    study_config = StudyConfig(job_name, dir_out, fastq_list, cufflinks_library_type, library_type, run,
-                               project=project)
+    study_config = StudyConfig(job_name, dir_out, fastq_list, None, library_type, run, project=project)
     study_config_path = study_config.parse(workflow=WORKFLOW_NAME)
     Launcher.launch(global_config_path, study_config_path)
 
